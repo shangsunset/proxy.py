34ecf606dbe0e78a818606452474a6b9df609e55
shangsunset <shangsunset@gmail.com>
2015-11-24 12:24:30 -0500

    proxy sort of working

--- proxy.py
+++ proxy.py
@@ -1,4 +1,5 @@
 import sys
+import errno
 import socket
 import thread
 
@@ -11,9 +12,10 @@ def start():
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(('', PORT))
         s.listen(MAX_CONNECTIONS)
-        print 'server started on {}...'.format(PORT)
+        print 'proxy server started on {}...'.format(PORT)
 
     except Exception:
         print 'unable to initialize socket...'
@@ -23,7 +25,10 @@ def start():
             connection, address = s.accept()
             print 'connection {}'.format(address)
             data = connection.recv(BUFFER_SIZE)
-            thread.start_new_thread(parse_request, (connection, address, data))
+            if data:
+                thread.start_new_thread(
+                    parse_request, (connection, address, data)
+                )
         except KeyboardInterrupt:
             s.close()
             print 'proxy server shutting down...'
@@ -31,10 +36,31 @@ def start():
 
 
 def parse_request(connection, address, data):
-    request_line = data.splitlines()[0]
+    request_line = data.splitlines()
+    request_line = request_line[0]
     request_line = request_line.rstrip('\r\n')
-    print data
     (request_method, path, request_version) = request_line.split()
+    path = path[len('http://'):]
+    host = path[:path.find('/')]
+
+    proxy_server(connection, address, data, host)
+
+
+def proxy_server(connection, address, data, host, port=80):
+    print 'host: {}'.format(host)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.send(data)
+
+    while True:
+        reply = s.recv(BUFFER_SIZE)
+        try:
+            connection.send(reply)
+        except socket.error as e:
+            if e.errno == errno.EPIPE:
+                print "Detected disconnection"
+    s.close()
+    connection.close()
 
 
 if __name__ == '__main__':
